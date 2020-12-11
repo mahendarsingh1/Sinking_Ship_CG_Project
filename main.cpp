54edeb48290ee1309af6a3369b4bd6aa28ab2c8f
Athish Venkatesh <athishaves@gmail.com>
2020-12-11 20:21:40 +0530

    Waves to ocean

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include<GL/glut.h>
 #include<stdlib.h>
 #include<time.h>
-#include<errno.h>
-#include<ctime>
-#include<cerrno>
+
+#include<math.h>
+#define PI 3.1416
 
 void rock();
 void display1();
@@ -12,18 +12,44 @@ void display2();
 void display3();
 void ship();
 void water();
-BOOLEAN nanosleep(LONGLONG);
 
-//struct timespec jmp, jmp2;
 
-GLint a=0,b=0,c=0,d=0,e=0,f=0,g=500,h=600,x=0,i=0;
-GLfloat theta=0.0;
+
+struct point {
+    GLfloat x,y,z;
+};
+
+void floatingWater();
+int factorial(int);
+void computeNcR(int, int*);
+void computeBezier(float, point *, int, point *, int *);
+void bezier(point *, int, int);
+
+
+struct timespec jmp,jmp2;
+
+GLfloat a=0,b=0,c=0,d=0,e=0,f=0,g=500,h=600,x=0,i=0;
+
+
+
+
+
+
+
+float theta = 0;
+
+
+
+
+
+
+
 
 void update(int value)
 {
-	a+=20.0;
+	a+=1;
 	glutPostRedisplay();
-	glutTimerFunc(200,update,0);
+	glutTimerFunc(10,update,0);
 }
 
 void display()
@@ -34,20 +60,21 @@ void display()
 	if(a>950)
 
 	{
-		b+=20;
+        if(b>180) b+=0.5;
+        else b+=1.5;
 		display2();
 	}
 	if(b==180)
 	{
-		//jmp.tv_sec = 2;
-		//jmp.tv_nsec = 500000000L;
-		//nanosleep(&jmp , &jmp2);
-        nanosleep(500000000L);
+        b+=1.5;
+		jmp.tv_sec = 0;
+		jmp.tv_nsec = 25000000L;
+		nanosleep(&jmp , &jmp2);
 	}
-	if(b>250)
+	if(b>180)
 	{
 
-		c+=10;
+		c+=0.75;
 		display3();
 
 	}
@@ -63,7 +90,7 @@ void display1()
 	glTranslated(a,75,0.0);
 	ship();
 	glPopMatrix();
-	water();
+	floatingWater();
 }
 
 void display2()
@@ -75,28 +102,149 @@ void display2()
 	glTranslated(b,75,0.0);
 	ship();
 	glPopMatrix();
-	water();
+//	water();
+	floatingWater();
 
 }
 
 void display3()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	x-=1;
+	x-=0.1;
 	rock();
 	glPushMatrix();
-	glTranslated(c,100+x*5,0.0);
-	glRotated(-15,0,0,1);
+	glTranslated(b,100+x*5,0.0);
+	glRotated(-10,0,0,1);
 	ship();
 	glPopMatrix();
-	water();
+//	water();
+	floatingWater();
 
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int factorial(int n) {
+    if(n<=1) return 1;
+    return n*factorial(n-1);
+}
+
+
+
+
+void computeNcR(int n, int *hold_ncr_values) {
+    int r;
+    for(r=0; r<=n; r++) hold_ncr_values[r] = factorial(n) / (factorial(n-r) * factorial(r));
+}
+
+
+
+
+void computeBezier(float t, point *actual_bezier_point, int n_main, point *control_points_array, int *hold_ncr_values) {
+    int i, n = n_main-1;
+    float bernstein_polynomial;
+    actual_bezier_point -> x = 0;
+    actual_bezier_point -> y = 0;
+    actual_bezier_point -> z = 0;
+    for(i=0; i<n_main; i++) {
+        bernstein_polynomial = hold_ncr_values[i] * pow(t,i) * pow(1-t,n-i);
+        actual_bezier_point -> x += bernstein_polynomial * control_points_array[i].x;
+        actual_bezier_point -> y += bernstein_polynomial * control_points_array[i].y;
+        actual_bezier_point -> z += bernstein_polynomial * control_points_array[i].z;
+    }
+}
+
+
+
+
+void bezier(point *control_points_array, int number_of_control_points, int number_of_bezier_points) {
+    point actual_bezier_point;
+    float t;
+    int *hold_ncr_values, i;
+    hold_ncr_values = new int[number_of_control_points];
+    computeNcR(number_of_control_points-1, hold_ncr_values);
+
+    glBegin(GL_LINE_STRIP);
+    for(int i=0; i<=number_of_bezier_points; i++) {
+        t = float(i)/float(number_of_bezier_points);
+        computeBezier(t, &actual_bezier_point, number_of_control_points, control_points_array, hold_ncr_values);
+        glVertex2f(actual_bezier_point.x, actual_bezier_point.y);
+    }
+    glEnd();
+    delete[] hold_ncr_values;
+}
+
+
+
+
+void floatingWater() {
+    int number_of_control_points=4, number_of_bezier_points=20;
+    float water_height = 125;
+    point control_points_array[number_of_control_points] = 
+                {{-250,water_height,0},{250,water_height+25,0},{750,water_height-25,0},{1250,water_height,0}};
+    control_points_array[1].x += 50*sin(theta*PI/180.0);
+    control_points_array[1].y += 25*sin(theta*PI/180.0);
+    control_points_array[2].x -= 50*sin((theta+30)*PI/180.0);
+    control_points_array[2].y -= 50*sin((theta+30)*PI/180.0);
+    control_points_array[3].x -= 25*sin((theta-30)*PI/180.0);
+    control_points_array[3].y += sin((theta-30)*PI/180.0);
+    theta += 2;
+    glPushMatrix();
+    glPointSize(5);
+    glEnable(GL_BLEND);
+    glBlendFunc(1, 0.5);
+    glColor3f(0.196078,0.6,0.8);
+    for(int i=0; i<200; i++) {
+        glTranslated(0,-0.8,0);
+        bezier(control_points_array, number_of_control_points, number_of_bezier_points);
+    }
+    glPopMatrix();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void water()
 {
-
+    glEnable(GL_BLEND);
+    glBlendFunc(1, 0.5);
 	glColor3f(0.196078,0.6,0.8);
 	glBegin(GL_POLYGON);
 		glVertex2f(0,0);
@@ -329,7 +477,7 @@ void ship()
 void rock()
 {
 	glPushMatrix();
-	glTranslated(450,50,0.0);
+	glTranslated(600,50,0.0);
 	glScaled(10,10,0);
 	glColor3f(0.36,0.25,0.20);
 
@@ -403,36 +551,15 @@ void myinit()
 	gluOrtho2D(0.0,999.0,0.0,799.0);
 }
 
-BOOLEAN nanosleep(LONGLONG ns){
-	/* Declarations */
-	HANDLE timer;	/* Timer handle */
-	LARGE_INTEGER li;	/* Time defintion */
-	/* Create timer */
-	if(!(timer = CreateWaitableTimer(NULL, TRUE, NULL)))
-		return FALSE;
-	/* Set timer properties */
-	li.QuadPart = -ns;
-	if(!SetWaitableTimer(timer, &li, 0, NULL, NULL, FALSE)){
-		CloseHandle(timer);
-		return FALSE;
-	}
-	/* Start & wait for timer */
-	WaitForSingleObject(timer, INFINITE);
-	/* Clean resources */
-	CloseHandle(timer);
-	/* Slept without problems */
-	return TRUE;
-}
-
 int main(int argc, char* argv[])
 
 {
 	int chs;
-	printf("Project Developed By cseminiprojects.com\n");
+	printf("Project Developed By Athish Venkatesh and Mahendar Singh Rathod\n");
+	// printf("\n------------------------------------------------------------------\n");
+	// printf("Steps to run this cg project\n");
 	printf("\n------------------------------------------------------------------\n");
-	printf("Steps to run this cg project\n");
-	printf("\n------------------------------------------------------------------\n");
-	printf("Step1: Type Any Key and Hit Enter\n");
+	printf("Type Any Key and Hit Enter\n");
 	printf("\n******************************************************************\n\n");
 	scanf("%d",&chs);
 
@@ -443,7 +570,7 @@ int main(int argc, char* argv[])
 	glutCreateWindow("Sinking Ship");
 	glutDisplayFunc(display);
 	myinit();
-	glutTimerFunc(100,update,0);
+	glutTimerFunc(250,update,0);
 	glutMainLoop();
 	return 0;
 }
